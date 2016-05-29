e63c96ca896c645a26d2e8e569c14f296ba05b7c
Aaron Siegel <siegelaaron94@gmail.com>
2016-05-28 23:57:18 -0700

    breadboard version.

--- src/main.c
+++ src/main.c
@@ -35,171 +35,65 @@
 #pragma config JTAGEN = OFF // JTAG Port Enable (JTAG port is disabled)
 
 #include <libpic30.h>
-#include <lighting.h>
 #include <xc.h>
-
-enum signal_light_state {
-    SIGNAL_OFF,
-    SIGNAL_RIGHT,
-    SIGNAL_LEFT,
-    SIGNAL_HAZARDS
-};
-
-enum head_light_state {
-    HEADLIGHTS_OFF,
-    HEADLIGHTS_LOW_BEAMS,
-    HEADLIGHTS_HIGH_BEAMS
-};
-
-void main_task()
-{
-    if (l_flg_tst_signal_light_state()) {
-        l_flg_clr_signal_light_state();
-        LATAbits.LATA0 = 0;
-        LATAbits.LATA1 = 0;
-    }
-
-    if (l_flg_tst_head_light_state()) {
-        switch ((enum head_light_state)l_u8_rd_head_light_state()) {
-        case HEADLIGHTS_OFF: {
-            LATAbits.LATA2 = 0;
-            LATAbits.LATA3 = 0;
-            break;
-        }
-        case HEADLIGHTS_LOW_BEAMS: {
-            LATAbits.LATA2 = 1;
-            LATAbits.LATA3 = 0;
-            break;
-        }
-        case HEADLIGHTS_HIGH_BEAMS: {
-            LATAbits.LATA2 = 1;
-            LATAbits.LATA3 = 1;
-            break;
-        }
-        }
-        l_flg_clr_head_light_state();
-    }
-}
 
 int main()
 {
-    // Initialize the LIN interface
-    if (l_sys_init())
-        return -1;
+    AD1PCFGL = 0xFFFF;
+    AD1PCFG = 0xFFFF;
 
-    // Initialize the interface
-    if (l_ifc_init_UART1())
-        return -1;
+    // Setup a 5ms timer
+    T1CONbits.TON = 1;
+    T1CONbits.TCKPS = 0b10;
+    T1CONbits.TCS = 0;
+    T1CONbits.TGATE = 0;
+    T1CONbits.TSYNC = 0;
+    PR1 = FCY / 128ul;
 
-    // Set UART TX to interrupt level 6
-    // Set UART RX to interrupt level 6
-    struct l_irqmask irqmask = { 6, 6 };
-    l_sys_irq_restore(irqmask);
-
-    l_bool configuration_ok = false;
-    l_u16 configuration_timeout = 1000;
-    do {
-        if (l_ifc_read_status_UART1() & (1 << 6)) {
-            configuration_ok = true;
-            break;
-        }
-        __delay_ms(5);
-        configuration_timeout--;
-    } while (configuration_timeout || !configuration_ok);
-
-    if (!configuration_ok) {
-        // Master did not configure this node.
-        return -1;
-    }
-
-    T2CONbits.TCS = 0b0; //Timer2 Clock Source is Internal the clock (FOSC/2)
-    T2CONbits.T32 = 0b0; //16 bit mode
-    T2CONbits.TGATE = 0b0; //Gated time accumulation is disabled
-    T2CONbits.TCKPS = 0b10; //Setting pre-scaler to 64
-
-    // (64)*PR2/FCY = 1/2
-    // PR2/FCY = 1/128
-    // PR2 = FCY/128
-    TMR2 = 0x0000;
-    PR2 = FCY / 128ul; // .5s delay
-
-    IEC0bits.T2IE = 1;
-    IPC1bits.T2IP = 5;
-    IFS0bits.T2IF = 0;
-    T2CONbits.TON = 1; //Turning timer2 on
+    IEC0bits.T1IE = 1;
+    IPC0bits.T1IP = 7;
 
     TRISAbits.TRISA0 = 0;
     TRISAbits.TRISA1 = 0;
     TRISAbits.TRISA2 = 0;
     TRISAbits.TRISA3 = 0;
+    TRISBbits.TRISB9 = 0;
+    TRISBbits.TRISB8 = 0;
+
+    TRISBbits.TRISB4 = 1;
+    TRISBbits.TRISB14 = 1;
+    TRISBbits.TRISB13 = 1;
+
+    TRISBbits.TRISB10 = 1;
+    TRISBbits.TRISB11 = 1;
+    TRISBbits.TRISB5 = 1;
+
+    TRISBbits.TRISB2 = 1;
 
     while (1) {
-        main_task();
+        LATAbits.LATA2 = PORTBbits.RB14;
+        LATAbits.LATA3 = PORTBbits.RB13;
+        LATBbits.LATB9 = PORTBbits.RB5;
+        LATBbits.LATB8 = PORTBbits.RB4;
+        __delay_ms(10);
     }
 
     return -1;
 }
 
-void __attribute__((interrupt, no_auto_psv)) _T2Interrupt()
+void __attribute__((interrupt, no_auto_psv)) _T1Interrupt()
 {
-    if (IFS0bits.T2IF) {
-        IFS0bits.T2IF = 0;
-        switch ((enum signal_light_state)l_u8_rd_signal_light_state()) {
-        case SIGNAL_OFF: {
-            LATAbits.LATA0 = 0;
+    if (IFS0bits.T1IF) {
+        IFS0bits.T1IF = 0;
+        if (PORTBbits.RB2 || PORTBbits.RB11) {
+            LATAbits.LATA1 = ~LATAbits.LATA1;
+        } else {
             LATAbits.LATA1 = 0;
-            break;
         }
-        case SIGNAL_RIGHT: {
+        if (PORTBbits.RB2 || PORTBbits.RB10) {
             LATAbits.LATA0 = ~LATAbits.LATA0;
-            break;
-        }
-        case SIGNAL_LEFT: {
-            LATAbits.LATA1 = ~LATAbits.LATA1;
-            break;
-        }
-        case SIGNAL_HAZARDS: {
-            LATAbits.LATA0 = ~LATAbits.LATA0;
-            LATAbits.LATA1 = ~LATAbits.LATA1;
-            break;
-        }
+        } else {
+            LATAbits.LATA0 = 0;
         }
     }
-}
-
-void __attribute__((interrupt, no_auto_psv)) _U1TXInterrupt()
-{
-    if (IFS0bits.U1TXIF) {
-        IFS0bits.U1TXIF = 0;
-        l_ifc_tx_UART1();
-    }
-}
-
-void __attribute__((interrupt, no_auto_psv)) _U1RXInterrupt()
-{
-    if (IFS0bits.U1RXIF) {
-        IFS0bits.U1RXIF = 0;
-        l_ifc_rx_UART1();
-    }
-}
-
-struct l_irqmask l_sys_irq_disable()
-{
-    IEC0bits.U1RXIE = 0;
-    IEC0bits.U1TXIE = 0;
-    IFS0bits.U1TXIF = 0;
-    IFS0bits.U1RXIF = 0;
-    struct l_irqmask mask = { IPC2bits.U1RXIP, IPC3bits.U1TXIP };
-    return mask;
-}
-
-void l_sys_irq_restore(struct l_irqmask previous)
-{
-    IPC2bits.U1RXIP = previous.rx_level;
-    IFS0bits.U1TXIF = 0;
-    IEC0bits.U1RXIE = 1;
-
-    IPC3bits.U1TXIP = previous.tx_level;
-    IFS0bits.U1RXIF = 0;
-    IEC0bits.U1TXIE = 1;
 }
